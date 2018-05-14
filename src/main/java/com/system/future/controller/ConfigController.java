15f934e299cf34fabae9291cb9084c409f9baccf
deanwang1943 <wangjingxin1943@163.com>
2018-05-14 21:12:22 +0800

    add more function to rest api

--- src/main/java/com/system/future/controller/ConfigController.java
+++ src/main/java/com/system/future/controller/ConfigController.java
@@ -2,6 +2,11 @@ package com.system.future.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.system.future.vo.InventoryVO;
+import com.system.future.vo.KeyVO;
+import com.system.future.vo.RepositoriesVO;
+import com.system.future.vo.TemplateVO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
@@ -18,6 +23,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/rest")
 @PropertySource("classpath:rest.properties")
+@Slf4j
 public class ConfigController {
     private static HttpHeaders headers = new HttpHeaders();
 
@@ -36,14 +42,6 @@ public class ConfigController {
     @Autowired
     private RestTemplate restTemplate;
 
-    public void login() {
-        final String url = "";
-        final String auth = "";
-        String result = this.restTemplate.postForEntity(url, auth, String.class).getBody();
-
-        System.out.println("Get result : " + result);
-    }
-
     private void setHeadersWithAuth() {
         headers.add("authorization", authCode);
     }
@@ -59,11 +57,9 @@ public class ConfigController {
         Map<String, Object> params = new HashMap<>();
         params.put("name", name);
         params.put("alert", true);
-        JSONObject itemJSONObj = JSONObject.parseObject(JSON.toJSONString(params));
-        setHeadersWithAuth();
-        final HttpEntity<JSONObject> formEntity = new HttpEntity<>(itemJSONObj, headers);
-        String result = projectHandler(HttpMethod.POST, formEntity);
-        System.out.println("Get all projects:" + result);
+        JSONObject jsonObject = JSONObject.parseObject(JSON.toJSONString(params));
+        String result = request(baseUrl + "projects", HttpMethod.POST, jsonObject);
+        log.info("Add project:" + result);
     }
 
     /**
@@ -71,18 +67,20 @@ public class ConfigController {
      */
     @GetMapping("/projects")
     public void getProjects() {
-        setHeadersWithAuth();
-        final HttpEntity<String> formEntity = new HttpEntity<>(null, headers);
-        String result = projectHandler(HttpMethod.GET, formEntity);
-        System.out.println("Get all projects:" + result);
-
+        String result = request(baseUrl + "projects", HttpMethod.GET);
+        log.info("Get all projects:" + result);
     }
 
-    private String projectHandler(HttpMethod method, HttpEntity formEntity) {
-        final String url = baseUrl + "projects";
+    private String request(final String url, final HttpMethod method) {
+        return request(url, method, null);
+    }
+
+    private String request(final String url, final HttpMethod method, JSONObject jsonObject) {
+        setHeadersWithAuth();
+        final HttpEntity<JSONObject> formEntity = new HttpEntity<>(jsonObject, headers);
         String result = restTemplate.exchange
                 (url, method, formEntity, String.class).getBody();
-        System.out.println("Get all projects:" + result);
+        log.info("Get response from request:" + result);
         return result;
     }
 
@@ -97,15 +95,37 @@ public class ConfigController {
      * "secret": "string"
      * }
      */
-    public void addProjectKeys() {
-
+    @PostMapping("/project/{projectId}/keys")
+    public void addProjectKeys(@PathVariable("projectId") Long projectId,@RequestBody KeyVO vo) {
+        final String url = baseUrl + "project/" + projectId + "/keys";
+        Map<String, Object> params = new HashMap<>();
+        params.put("name", vo.getName());
+        params.put("type", "ssh");
+        params.put("project_id", projectId);
+        params.put("key", vo.getKey());
+        params.put("secret", vo.getSecret());
+        JSONObject jsonObject = JSONObject.parseObject(JSON.toJSONString(params));
+        String result = request(url, HttpMethod.POST, jsonObject);
     }
 
     /**
-     *
+     * {
+     * "name": "string",
+     * "project_id": 0,
+     * "git_url": "string",
+     * "ssh_key_id": 0
+     * }
      */
-    public void addProjectRepo() {
-
+    @PostMapping("/project/{projectId}/repositories")
+    public void addProjectRepo(@PathVariable("projectId") Long projectId, @RequestBody RepositoriesVO vo) {
+        final String url = baseUrl + "project/" + projectId + "/repositories";
+        Map<String, Object> params = new HashMap<>();
+        params.put("name", vo.getName());
+        params.put("git_url", vo.getGitUrl());
+        params.put("project_id", projectId);
+        params.put("ssh_key_id", vo.getSshKeyId());
+        JSONObject jsonObject = JSONObject.parseObject(JSON.toJSONString(params));
+        String result = request(url, HttpMethod.POST, jsonObject);
     }
 
     /**
@@ -121,8 +141,18 @@ public class ConfigController {
      * "type": "static"
      * }
      */
-    public void addProjectInve() {
-
+    @PostMapping("/project/{projectId}/inventory")
+    public void addProjectInve(@PathVariable("projectId") Long projectId,@RequestBody InventoryVO vo) {
+        final String url = baseUrl + "project/" + projectId + "/inventory";
+        Map<String, Object> params = new HashMap<>();
+        params.put("name", vo.getName());
+        params.put("inventory", vo.getInventory());
+        params.put("project_id", projectId);
+        params.put("key_id", vo.getKeyId());
+        params.put("ssh_key_id", vo.getSshKeyId());
+        params.put("type", "static");
+        JSONObject jsonObject = JSONObject.parseObject(JSON.toJSONString(params));
+        String result = request(url, HttpMethod.POST, jsonObject);
     }
 
     /**
@@ -141,7 +171,39 @@ public class ConfigController {
      * "override_args": true
      * }
      */
-    public void addProjectTemp() {
+    @PostMapping("/project/{projectId}/templates")
+    public void addProjectTemp(@PathVariable("projectId") Long projectId,@RequestBody TemplateVO vo) {
+        final String url = baseUrl + "project/" + projectId + "/templates";
+        Map<String, Object> params = new HashMap<>();
+        params.put("inventory_id", vo.getInventoryId());
+        params.put("project_id", projectId);
+        params.put("repository_id", vo.getRepositoryId());
+        params.put("ssh_key_id", vo.getSshKeyId());
+        params.put("environment_id", vo.getEnvironmentId());
+        params.put("alias", vo.getAlias());
+        params.put("playbook", vo.getPlaybook());
+        params.put("arguments", vo.getArguments());
+        params.put("override_args", true);
+        JSONObject jsonObject = JSONObject.parseObject(JSON.toJSONString(params));
+        String result = request(url, HttpMethod.POST, jsonObject);
+    }
 
+    /**
+     * project_id
+     * {
+     * "template_id": 0,
+     * "debug": true,
+     * "dry_run": true,
+     * "playbook": "string",
+     * "environment": "string"
+     * }
+     */
+    @PostMapping("/project/{projectId}/tasks")
+    public void startJobs(@PathVariable("projectId") Long projectId) {
+        final String url = baseUrl + "project/" + projectId + "/tasks";
+        String result = request(url, HttpMethod.GET);
+        log.info(result);
+        JSONObject jsonObject = JSONObject.parseObject(JSON.toJSONString(result));
+        result = request(url, HttpMethod.POST, jsonObject);
     }
 }
